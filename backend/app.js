9aac752056f71dd74cf856f380daa65729302c66
collapsly <collapsly@protonmail.com>
2023-01-15 23:54:39 +0300

    try to enable CORS for particular function

--- backend/app.js
+++ backend/app.js
@@ -21,30 +21,30 @@ const app = express();
 app.use(express.static(path.resolve('.') + '/frontend/public'));
 app.use(express.json());
 
-app.get('/api/participants', async (_req, res) => {
-    const participants = await Participant.find();
-
-    res.send(participants);
-});
-
-app.post('/api/participants', async (req, res) => {
-    const participant = new Participant(req.body);
-    await participant.save();
-
-    res.send(participant);
-});
-
-app.delete('/api/participants/:id', async (req, res) => {
-    await Participant.findByIdAndDelete(req.params.id);
-
-    res.send('ok');
-});
-
-app.delete('/api', async (_req, res) => {
-    await Participant.remove();
-
-    res.send('ok');
-});
+// app.get('/api/participants', async (_req, res) => {
+//     const participants = await Participant.find();
+//
+//     res.send(participants);
+// });
+//
+// app.post('/api/participants', async (req, res) => {
+//     const participant = new Participant(req.body);
+//     await participant.save();
+//
+//     res.send(participant);
+// });
+//
+// app.delete('/api/participants/:id', async (req, res) => {
+//     await Participant.findByIdAndDelete(req.params.id);
+//
+//     res.send('ok');
+// });
+//
+// app.delete('/api', async (_req, res) => {
+//     await Participant.remove();
+//
+//     res.send('ok');
+// });
 
 app.use((_req, _res, next) => {
     next(createError(404));
