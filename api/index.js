388ac5892de5b7114ee6f10d00cf2d19f2b6cb6f
collapsly <collapsly@protonmail.com>
2023-01-16 13:34:25 +0300

    use of serverless endpoints

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Participant } from '../backend/models/Participant.js';
+
 
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://127.0.0.1/proga_db';
 
@@ -7,3 +9,19 @@ mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true 
 
 const db = mongoose.connection;
 db.on('error', console.error);
+
+
+export default function(req, res) {
+    if (req.method === 'DELETE') {
+        return dropDB(req, res);
+    }
+
+    res.status(404).send('Not Found');
+}
+
+
+async function dropDB(_req, res) {
+    await Participant.remove();
+
+    res.send('ok');
+}
--- api/participants.js
+++ api/participants.js
@@ -1,35 +1,16 @@
 import './index.js';
+
 import { Participant } from '../backend/models/Participant.js';
-
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-
-    return await fn(req, res)
-}
-
-// export default allowCors((request, response) => {
-//     response.send('ok');
-// });
 
 
 export default function(req, res) {
     if (req.method === 'GET') {
-        getParticipants(req, res);
+        return getParticipants(req, res);
     } else if (req.method === 'POST') {
-        createParticipant(req, res);
+        return createParticipant(req, res);
     }
+
+    res.status(404).send('Not Found');
 }
 
 
